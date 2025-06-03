1b8fc8cfd202a1dcbc951b9c4e8a4666dcc4519c
michiww <emmanuelasembeng026@student.unsrat.ac.id>
2025-06-03 15:22:22 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,146 @@
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Emma's Profile</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+
+
+    <div class="main">
+        <nav>
+            <h1><i>Here's some information about me</i></h1>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#gallery">Gallery</a></li>
+                <li><a href="#blog">Blog</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+</body>
+</html>
+
+
+<section id="home">
+    <div>
+        <br/><hr><br/><br/>
+        <h2>Hi this is me!</h2>
+        <img src="michiw.jpg" width="350">
+        <p>an ordinary girl with limited abilities who likes to find out things and then decided to learn computer programs and still maintain a hobby in art while still living a relaxed and happy life❀⋆.ೃ࿔*:･･</p>
+          <br/>
+        </div>
+</section>
+
+<section id="gallery">
+    <div>
+        <br/><hr>
+        <h2>My Gallery</h2>
+        <img src="Yuki.jpeg" class="gallery-img" width="300">
+        <img src="michi.jpg" class="gallery-img" width="300">
+        <img src="paint.jpeg" class="gallery-img" width="245">
+    </div>
+</section>
+
+
+<section id="blog">
+</section>
+    <br/><hr>
+    <h2>Blog</h2>
+        <h3>BTS Pecahkan Rekor 17 Tahun Destiny's Child di Billboard Music Award</h3>
+        <p>BTS menjadi grup paling banyak mendapatkan penghargaan dalam sejarah Billboard Music Awards sejak kemenangan pertama mereka pada 2017 silam.
+            Pada ajang Billboard Music Awards 2022 Senin (16/5), dari enam nominasi, BTS mendapatkan tiga penghargaan yaitu Top Duo/Group, Top Song Sales Artist dan Top Selling Song untuk 'Butter'.
+            
+            <br/><br/>Dengan tambahan tiga gelar tersebut, BTS kini mengoleksi 11 gelar. Mengutip The Independent, BTS kini menggeser rekor yang sebelumnya dipegang oleh Destiny's Child, yang memenangkan total 11 penghargaan.
+            
+            <br/><br/>Rekor Destiny's Child tersebut telah bertahan selama 17 tahun. BTS, grup yang terdiri dari RM, Jin, Suga, J-Hope, Jimin, V dan Jungkook, juga telah memenangkan trofi di setiap ajang BBMA sejak 2017.
+            
+            <br/><br/>BTS Army, sebutan untuk penggemar grup, merayakan perjalanan bersejarah grup tersebut di BBMA di Twitter dengan pesan dan postingan ucapan selamat.
+            
+            <br/><br/>Tahun ini BBMA diadakan di MGM Grand Garden Arena, Las Vegas, dengan rapper P Diddy menjadi pembawa acara.
+            
+            <br/><br/>Selain BTS, The Weeknd juga menjadi sorotan dengan mendapatkan nominasi penghargaan terbanyak tahun ini, yaitu total nominasi di 17 kategori. Di urutan kedua ada Doja Cat yang memiliki 14 nominasi.
+            
+            <br/><br/>Justin Bieber, Kanye West, dan Olivia Rodrigo masing-masing menerima 13 nominasi, sementara Drake dinominasikan dalam 11 kategori.
+            
+            <br/><br/>Drake sendiri mendapat kemenangan besar dengan membawa pulang lima penghargaan - termasuk Top Rap Artist dan Top Rap Album - untuk albumnya Certified Lover Boy yang dirilis tahun lalu.
+            
+            <br/><br/><i>Source : <a href="https://www.cnnindonesia.com/hiburan/20220516130446-227-797219/bts-pecahkan-rekor-17-tahun-destinys-child-di-billboard-music-award">https://www.cnnindonesia.com</a></i> 
+            </p><hr/>
+            <h3>Jalani Wajib Militer, V BTS Bagikan Kabar Terbaru</h3>
+        <p>V BTS mengalami kenaikan berat badan lebih dari 10 kg setelah masuk wajib militer. Pada 14 Februari 2024, V mengejutkan penggemar dengan kembali ke Weverse dan membagikan kabar terbarunya.  
+
+            <br/><br/>V mendaftar wajib militer pada 11 Desember 2023 melalui Pusat Pelatihan Angkatan Darat Nonsan. Saat ini, ia bertugas di Divisi Infanteri ke-2, Unit Ssangyong, di Chuncheon, Provinsi Gangwon. Meski aktivitasnya terbatas, tentara diperbolehkan menggunakan ponsel dalam waktu tertentu, sehingga V dapat berkomunikasi dengan penggemar.  
+            
+            <br/><br/>Dalam unggahannya, V mengungkapkan berat badannya kini mencapai 72,5 kg. Sebelumnya, ia memiliki tubuh ramping dengan berat 62 kg dan tinggi 179 cm. Pada 2022, penggemar sempat khawatir saat berat badannya hanya 63,4 kg.  
+            
+            <br/><br/>Sebelum wamil, V sempat mengumumkan niatnya untuk meningkatkan berat badan hingga 86 kg guna memperkuat fisiknya. Ia juga meyakinkan rekan-rekan BTS bahwa ia akan kembali dengan tubuh lebih kekar.  
+    
+            <br/><br/>Penggemar antusias menantikan perubahan penampilannya, bahkan berspekulasi bahwa ia bisa melampaui target 80 kg. Dengan tinggi badannya yang semampai, perubahan fisik ini semakin menarik perhatian.  
+            
+            <br/><br/>V BTS dijadwalkan menyelesaikan wajib militernya pada 10 Juni 2025.
+            
+            <br/><br/><i>Source : <a href="https://www.tempo.co/hiburan/jalani-wajib-militer-v-bts-bagikan-kabar-terbaru-berat-badannya-naik-10-kilogram-86594">https://www.tempo.co</a></i>
+        </p><hr/>
+        <h3>J-hope BTS Berikan Donasi 200 Juta Won untuk Penelitian Penyakit Langka Anak-anak</h3>
+        <p>J-hope BTS merayakan ulang tahunnya dengan memberikan kembali cinta yang telah diterimanya dengan cara yang bermakna. Pada tanggal 18 Februari, untuk merayakan ulang tahunnya, j-hope menyumbangkan 200 juta won (sekitar $138.508) ke Asan Medical Center di Seoul.
+
+            <br/><br/>Dana tersebut nantinya akan meningkatkan fasilitas dan perawatan medis untuk pasien anak-anak dan remaja serta mendukung penelitian tentang penyakit anak-anak yang langka dan serius.
+            
+            <br/><br/>J-hope mengungkapkan pemikirannya tentang sumbangan tersebut dengan harapakan agar donasinya tersebut membantu anak-anak yang sedang menderita.
+            
+            <br/><br/>"Saya harap sumbangan ini dapat memberikan sedikit bantuan kepada anak-anak yang sedang menderita saat ini. Saya dengan tulus berharap mereka memiliki masa depan yang sehat dan penuh harapan," ungkap J-hope.
+            
+            <br/><br/>Anggota BTS tersebut juga berbagi keinginannya untuk terus mendukung tujuan tersebut."Saya ingin memberikan kembali cinta yang selalu ditunjukkan ARMY kepada saya. Dimulai dengan ulang tahun ini, saya merasa terhormat untuk terus berdonasi setiap tahun," lanjutnya.
+            
+            <br/><br/>J-hope secara konsisten menunjukkan komitmennya untuk memberi kembali. Kontribusinya baru-baru ini termasuk 100 juta won (sekitar $69.224) untuk mendukung korban tragedi kecelakaan pesawat pada bulan Januari, 100 juta won untuk bantuan gempa bumi Turki-Suriah pada tahun 2023, 100 juta won untuk Seoul Asan Medical Center pada tahun 2022, dan 100 juta won lainnya untuk mendukung anak-anak kurang mampu pada tahun 2021.
+            <br/><br/><i>Source : <a href="https://www.grid.id/read/044218645/rayakan-ulang-tahun-j-hope-bts-berikan-donasi-200-juta-won-untuk-penelitian-penyakit-langka-anak-anak">https://www.grid.id</a></i>
+        </p>
+</section>
+
+<section id="contact">
+    <hr/>
+    <h2>Contact</h2>
+    <h4>Want to know more about me? please contact me here (˶˃ ᵕ ˂˶).ᐟ.ᐟ</h4>
+    <link rel="stylesheet" href="index.css">
+    <div class="contact-row">
+        <div class="contact-item">
+            <img src="el.jpeg" class="icon-img" alt="Email Icon">
+            <span>emmanuelamichiko@gmail.com</span>
+        </div>
+
+        <div class="contact-item">
+            <img src="wa.jpeg" class="icon-img" alt="WhatsApp Icon">
+            <a href="https://wa.me/6285240088357" target="_blank">+62 852 4008 8357</a>
+        </div>
+
+        <div class="contact-item">
+            <img src="ig.jpeg" class="icon-img" alt="Instagram Icon">
+            <a href="https://www.instagram.com/emmnuela_04" target="_blank">@emmnuela_04</a>
+        </div>
+    </div>
+
+    <h4>Thanks for visiting my website :3</h4>
+    <hr>
+   
+    
+
+</section>
+
+<section id="bek">
+    <a href="index.html">Back</a>
+</section>
+
+<script src="index.js"></script>
+</body>
+</html>
